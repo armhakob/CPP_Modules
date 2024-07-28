ad7f78cd0cfb1ae763ea9e9871c44d2c87f8b46d
armhakob <hakobyan.armine2004@gmail.com>
2024-07-28 17:49:37 +0400

    Modul03

---
+++ Modul03/ex01/incs/ScavTrap.hpp
@@ -0,0 +1,18 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "../incs/"
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap(void);
+		ScavTrap(std::string const &name);
+		ScavTrap(ScavTrap const &copy);
+		ScavTrap	&operator=(ScavTrap const &copy);
+		~ScavTrap(void);
+		void		guardGate(void);
+		void		attack(const std::string& target);
+};
+
+#endif
