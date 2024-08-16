622af6d62a65ef3c05597490c6c7593bb4c2c981
Armine Hakobyan <armhakob@c2r5s2.42yerevan.am>
2024-08-16 12:02:28 +0400

    fixed ex02'

--- Module04/ex02/incs/Cat.hpp
+++ Module04/ex02/incs/Cat.hpp
@@ -2,14 +2,18 @@
 # define CAT_HPP
 
 #include "AAnimal.hpp"
+#include "Brain.hpp"
 
 class Cat : public AAnimal
 {
+	private:
+		Brain *brain;
     public:
         Cat(void);
         Cat(Cat const &copy);
         ~Cat(void);
         Cat &operator=(Cat const &copy);
+		Brain *getBrain(void) const;
         void makeSound(void) const;
 };
 
--- Module04/ex02/src/Cat.cpp
+++ Module04/ex02/src/Cat.cpp
@@ -3,23 +3,30 @@
 Cat::Cat(void): AAnimal("Cat")
 {
 	std::cout << "Cat's default constructor called" << std::endl;
+	brain = new Brain();
 }
 
 Cat::~Cat(void)
 {
 	std::cout << "Cat's destructor called" << std::endl;
+	delete brain;
 }
 
 Cat::Cat(Cat const &copy): AAnimal(copy)
 {
 	std::cout << "Cat's copy constructor called" << std::endl;
+	brain = new Brain(*copy.brain);
 }
 
 Cat	&Cat::operator=(Cat const &copy)
 {
 	std::cout << "Cat's assignment operator called" << std::endl;
 	if (this != &copy)
+	{
 		this->_type = copy._type;
+		delete brain;
+		brain = new Brain(*copy.brain);
+	}
 	return (*this);
 }
 
