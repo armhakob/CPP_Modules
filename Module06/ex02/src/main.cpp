b62b0a406359f9e7d5508a21e8449fd416924535
Armine Hakobyan <armhakob@c1r4s1.42yerevan.am>
2024-09-24 14:09:04 +0400

    ex00 maybe final version

--- Module06/ex02/src/main.cpp
+++ Module06/ex02/src/main.cpp
@@ -3,17 +3,17 @@
 int	main(void)
 {
 	Base *p = generate();
-    Base *a = new A;
-    Base *b = new B;
-    Base *c = new C;
+	Base *a = new A;
+	Base *b = new B;
+	Base *c = new C;
 
-    identify(p);
-    identify(*a);
-    identify(*b);
-    identify(*c);
+	identify(p);
+	identify(*a);
+	identify(*b);
+	identify(*c);
 
-    delete p;
-    delete a;
-    delete b;
-    delete c;
+	delete p;
+	delete a;
+	delete b;
+	delete c;
 }
